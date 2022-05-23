6d7345a662115563afc5f5dda761b77befcb748d
seanbreaksintotech <sbyrdie123@gmail.com>
2022-05-23 12:52:38 -0400

    new message

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Body from "./components/body";
 import Howitworks from "./components/howitworks";
+import Footer from "./components/footer";
 function App() {
   return (
     <>
       <Navbar />
       <Body />
       <Howitworks />
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footcontainer">
+        <div className="row">
+          <div className="col">
+            <h4>DOGGO DELIVERY</h4>
+            <ul>
+              <li>Contact Us</li>
+              <li>Socials</li>
+              <li>Prices</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col">
+          <h4>DOGGO DELIVERY</h4>
+          <ul>
+            <li>Contact Us</li>
+            <li>Socials</li>
+            <li>Prices</li>
+          </ul>
+        </div>
+
+        <div className="col">
+          <h4>DOGGO DELIVERY</h4>
+          <ul>
+            <li>Contact Us</li>
+            <li>Socials</li>
+            <li>Prices</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
